dcdbdf2526384b1e93fc04626938f22a8d3d42c5
Adam Duke <adam.v.duke@gmail.com>
2025-03-18 11:54:08 -0400

    add version and help flags

--- main.go
+++ main.go
@@ -8,8 +8,14 @@ import (
 	"github.com/adamvduke/bcrypt-cli/commands"
 )
 
+const (
+	version = "0.0.1"
+)
+
 func main() {
 	app := kingpin.New("bcrypt-cli", "Wraps golang.org/x/crypto/bcrypt in a cli")
+	app.Version(version).VersionFlag.Short('v')
+	app.HelpFlag.Short('h')
 	commands.ConfigureCompareCommand(app, os.Stdin, os.Stdout)
 	commands.ConfigureCostCommand(app, os.Stdin, os.Stdout)
 	commands.ConfigureHashCommand(app, os.Stdin, os.Stdout)
